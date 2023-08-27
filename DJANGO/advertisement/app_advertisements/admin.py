e59260f830f01dd5bfba462db9a60dca654d74c3
Nick <nick@gmail.com>
2023-08-27 21:52:21 +0300

    init

--- DJANGO/advertisement/app_advertisements/admin.py
+++ DJANGO/advertisement/app_advertisements/admin.py
@@ -1,21 +1,22 @@
 from django.contrib import admin
-from .models import Advertisements
+from .models import Advertisement
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ["id", "title", "description", "price", "created_at", "auction"]
+    list_display = ["id", "title", "description", "price", "created_date", "auction", "uptated_date"]
     list_filter = ["auction", "created_at"]
     actions = ["make_auction_as_false", "make_auction_as_true"]
     fieldsets = (
         ("Общие", {"fields": ("title", "description")}),
-        ("Финансы", {"fields": ("price", "auction")})
+        ("Финансы", {"fields": ("price", "auction"), "classes": ["collapse"]})
     )
 
-    @admin.action(description="Убрать возможность тогра")
+
+    @admin.action(description="Убрать возможность торга")
     def make_auction_as_false(self, request, queryset):
         queryset.update(auction=False)
 
-    @admin.action(description="Добавить возможность тогра")
+    @admin.action(description="Добавить возможность торга")
     def make_auction_as_true(self, request, queryset):
         queryset.update(auction=True)
 
-admin.site.register(Advertisements, AdvertisementAdmin)
+admin.site.register(Advertisement, AdvertisementAdmin)
--- DJANGO/advertisement/app_advertisements/models.py
+++ DJANGO/advertisement/app_advertisements/models.py
@@ -1,20 +1,37 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
 
-class Advertisements(models.Model):
+
+class Advertisement(models.Model):
+    title = models.CharField("заголовок", max_length=128)
+    description = models.TextField("описание")
+    price = models.DecimalField("цена", max_digits=10, decimal_places=2)
+    auction = models.BooleanField("торг", help_text="Отметьте, если торг уместен")
+    created_at = models.DateTimeField(auto_now_add=True)
+    uptated_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description='дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+
+    @admin.display(description='дата последнего обновления')
+    def uptated_date(self):
+        from django.utils import timezone
+        if self.uptated_at.date() == timezone.now().date():
+            created_time = self.uptated_at.time().strftime("%H:%M:%S")
+            return format_html(
+                '<span style="color: red; font-weight: bold;">Сегодня в {}</span>', created_time
+            )
+        return self.uptated_at.strftime("%d.%m.%Y в %H:%M:%S")
+
     class Meta:
         db_table = 'advertisements'
-
-    title = models.CharField("Заголовок", max_length=128)
-    description = models.TextField("Описание")
-    price = models.DecimalField("Цена", max_digits=8, decimal_places=2)
-    auction = models.BooleanField("Торг", help_text="Отметьте, если будет уместен торг")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
     def __str__(self):
         return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
-
-    class Meta:
-        db_table = 'advertisements'
-
-
